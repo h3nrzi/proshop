0cbc18f46f46c9c46436425e5979ae27677cc545
Hossein <rezaeig22@gmail.com>
2024-06-25 02:44:28 +0330

    Implement error handler

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -1,7 +1,12 @@
 import { Express } from "express";
+import errorHandler from "../controllers/error-handler";
+import notFound from "../middlewares/notFound";
 import productRoutes from "../routes/product-router";
 
 module.exports = (app: Express) => {
   app.get("/", (req, res) => res.status(200).send("API running..."));
   app.use("/api/products", productRoutes);
+  app.use("*", notFound);
+
+  app.use(errorHandler);
 };
