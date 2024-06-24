af80a67252be16759924ea47860fcba306bc0ba2
Hossein <rezaeig22@gmail.com>
2024-06-24 19:27:55 +0330

    Modeling product & user & order

---
+++ backend/models/user.ts
@@ -0,0 +1,44 @@
+import { model, Schema } from "mongoose";
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+  comparePassword(enteredPassword: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<User>(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [2, "Name must be at least 2 characters long"],
+      maxlength: [50, "Name cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+export default User;
