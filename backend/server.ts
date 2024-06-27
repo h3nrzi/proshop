aa45dc3b62a1087b5103dc83e0cbb26ca11a108e
Hossein <rezaeig22@gmail.com>
2024-06-27 11:51:17 +0330

    Config file

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import { Express } from "express";
 const app = require("express")() as Express;
 
 require("./start/db")();
+require("./start/config")(app);
 require("./start/routes")(app);
 
 const port = process.env.PORT || 3000;
