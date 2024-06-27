cbe48c5a4867fb54f872d3258c7e8afc2717c716
Hossein <rezaeig22@gmail.com>
2024-06-27 14:33:44 +0330

    User profile endpoints

--- backend/controllers/user-controller.ts
+++ backend/controllers/user-controller.ts
@@ -1,4 +1,7 @@
 import { RequestHandler } from "express";
+import _ from "lodash";
+import UpdateUserProfile from "../dtos/User/UpdateUserProfile";
+import User from "../models/user";
 import CustomRequest from "../types/CustomRequest";
 
 // @desc    Get users
@@ -24,9 +27,32 @@ export const deleteUser: RequestHandler = async (req, res, next) => {};
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
-export const getUserProfile: RequestHandler = async (req: CustomRequest, res, next) => {};
+export const getUserProfile: RequestHandler = async (req: CustomRequest, res, next) => {
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  return res.status(200).json(_.pick(user, "_id", "name", "email", "isAdmin"));
+};
 
 // @desc    Update user profile
 // @route   PATCH /api/users/profile
 // @access  Private
-export const updateUserProfile: RequestHandler = async (req: CustomRequest, res, next) => {};
+export const updateUserProfile: RequestHandler = async (req: CustomRequest, res, next) => {
+  const { email, name, password } = req.body as UpdateUserProfile;
+
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  user.name = name || user.name;
+  user.email = email || user.email;
+  if (password) user.password = password!;
+  const updatedUser = await user.save();
+
+  return res.status(200).json(_.pick(updatedUser, "_id", "name", "email", "isAdmin"));
+};
---
+++ backend/dtos/User/UpdateUserProfile.ts
@@ -0,0 +1,5 @@
+export default interface UpdateUserProfile {
+  name?: string;
+  email?: string;
+  password?: string;
+}
